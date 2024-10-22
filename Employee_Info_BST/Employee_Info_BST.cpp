2d830db5629a4605640bc3f1cce66f636359c902
Jonah Ebent <44449970+jebent521@users.noreply.github.com>
2024-10-22 18:05:09 -0400

    Changed implementation of BST

--- Employee_Info_BST/Employee_Info_BST.cpp
+++ Employee_Info_BST/Employee_Info_BST.cpp
@@ -28,160 +28,206 @@ driver using your “add" function, where salary ranges are from 30000 and 20000
 Then, print the menu for users to choose from.
 */
 #include <iostream>
+#include <functional>
 #include <string>
 #include <vector>
 
 using namespace std;
 
-/* Objects of the Employee class are nodes in the BST */
-class Employee {
-public:
-    Employee(int salary, string firstName, string lastName, string jobTitle):
-        salary(salary),
+struct Emp {
+    Emp() {};
+    Emp(string firstName, string lastName, string jobTitle, int salary) :
         firstName(firstName),
         lastName(lastName),
-        jobTitle(jobTitle) {}
+        jobTitle(jobTitle),
+        salary(salary) {}
 
-    // The node's parent
-    Employee* p = nullptr;
-    // The node's left child
-    Employee* left = nullptr;
-    // The node's right child
-    Employee* right = nullptr;
-
-    // tree priority (doubling as satellite data)
     int salary;
-
-    // satellite data
     string firstName;
     string lastName;
     string jobTitle;
 
-    friend ostream& operator<<(ostream& os, const Employee& obj) {
-        os << obj.firstName << " " << obj.lastName << " - " << obj.jobTitle << " ($" << obj.salary << ")";
-        return os;
+    bool operator<(const Emp& other) const {
+        return salary < other.salary;
     }
 
-    bool operator==(const Employee& other) const {
+    bool operator<=(const Emp& other) const {
+        return salary <= other.salary;
+    }
+
+    bool operator>(const Emp& other) const {
+        return salary > other.salary;
+    }
+
+    bool operator>=(const Emp& other) const {
+        return salary >= other.salary;
+    }
+    
+    bool operator==(const Emp& other) const {
         return salary == other.salary
             && firstName == other.firstName
             && lastName == other.lastName
             && jobTitle == other.jobTitle;
     }
+
+    bool operator!=(const Emp& other) const {
+        return !(*this == other);
+    }
+
+    friend ostream& operator<<(ostream& os, const Emp& obj) {
+        os << obj.firstName << " " << obj.lastName << ", " << obj.jobTitle << " ($" << obj.salary << ")";
+        return os;
+    }
 };
 
-/* A binary search tree that contains Employees */
-class EmployeeBST {
-private:
-    Employee* minimum(Employee* e) {
-        while (e->left != nullptr) {
-            e = e->left;
+class BST {
+
+    struct node {
+        node(Emp data) : data(data), left(nullptr), right(nullptr) {}
+
+        Emp data;
+        node* left;
+        node* right;
+    };
+
+    node* root;
+
+    node* makeEmpty(node* t) {
+        if (t == nullptr)
+            return nullptr;
+        {
+            makeEmpty(t->left);
+            makeEmpty(t->right);
+            delete t;
         }
-        return e;
+        return nullptr;
     }
 
-    Employee* maximum(Employee* e) {
-        while (e->right != nullptr) {
-            e = e->right;
+    node* insert(Emp x, node* t) {
+        if (t == nullptr) {
+            t = new node(x);
+            // t->data = x;
+            // t->left = t->right = nullptr;
         }
-        return e;
+        else if (x < t->data) {
+            t->left = insert(x, t->left);
+        }
+        else {
+            t->right = insert(x, t->right);
+        }
+        return t;
     }
 
-    Employee* successor(Employee* e) {
-        // if e has a right child, return minimum of right subtree
-        if (e->right != nullptr) return minimum(e->right);
-
-        // else, return first parent greater than e
-        Employee* parent = e->p;
-        while (parent != nullptr && e == parent->right) {
-            e = parent;
-            parent = parent->p;
-        }
-        return parent;
+    node* findMin(node* t) {
+        if (t == nullptr) return nullptr;
+        else if (t->left == nullptr) return t;
+        else return findMin(t->left);
     }
 
-    void transplant(Employee* u, Employee* v) {
-        if (u->p == nullptr) root = v;
-        else if (u == u->p->left) u->p->left = v;
-        else u->p->right = v;
+    node* findMax(node* t) {
+        if (t == nullptr) return nullptr;
+        else if (t->right == nullptr) return t;
+        else return findMax(t->right);
+    }
+
+    node* remove(Emp x, node* t) {
+        node* temp;
+        if (t == nullptr) return nullptr;
+        else if (x != t->data) { // traverse down the tree
+            if (x < t->data) {
+                t->left = remove(x, t->left);
+            }
+            else if (x > t->data) {
+                t->right = remove(x, t->right);
+            }
+        }
+        // found a node with the same salary
+        else if (t->left && t->right) {         // if it has two children
+            temp = findMin(t->right);
+            t->data = temp->data;
+            t->right = remove(t->data, t->right);
+        }
+        else {                                  // if it has one
+            temp = t;
+            if (t->left == nullptr) {
+                t = t->right;
+            }
+            else if (t->right == nullptr) {
+                t = t->left;
+            }
+            delete temp;
+        }
+
+        return t;
+    }
+
+    void inorder(node* t) {
+        if (t == nullptr)
+            return;
+        inorder(t->left);
+        cout << t->data << endl;
+        inorder(t->right);
+    }
+
+    void inorderConditional(node* t, function<bool(node *)> condition) {
+        if (t == nullptr || !condition(t)) return;
+        inorderConditional(t->left, condition);
+        cout << t->data << endl;
+        inorderConditional(t->right, condition);
+    }
+
+    node* find(node* t, int x) {
+        if (t == nullptr) return nullptr;
+        else if (x < t->data.salary) return find(t->left, x);
+        else if (x > t->data.salary) return find(t->right, x);
+        else return t;
     }
 
 public:
-    EmployeeBST() {}
-
-    // The root of the tree, initialized to NIL
-    Employee* root = nullptr;
-
-    void insert(Employee employee) {
-        // start at the root
-        Employee* current = root;
-        Employee* parent = nullptr;
-        // traverse down the tree
-        while (current != nullptr) {
-            parent = current;
-            if (employee.salary < current->salary) {
-                current = current->left;
-            }
-            else {
-                current = current->right;
-            }
-        }
-        // establish tree relationships
-        employee.p = parent;
-        if (parent == nullptr) root = &employee;
-        else if (employee.salary < parent->salary) parent->left = &employee;
-        else parent->right = &employee;
+    BST() {
+        root = nullptr;
     }
 
-    void remove(Employee* z) {
-        if (z->left == nullptr) transplant(z, z->right);
-        else if (z->right == nullptr) transplant(z, z->left);
-        else {
-            Employee* y = successor(z);
-            if (y != z->right) {
-                transplant(y, y->right);
-                y->right = z->right;
-                y->right->p = y;
-            }
-            transplant(z, y);
-            y->left = z->left;
-            y->left->p = y;
-        }
+    ~BST() {
+        root = makeEmpty(root);
     }
 
-    Employee* search(Employee* e, int salary) {
-        if (e == nullptr || e->salary == salary) return e;
-        if (e->salary < salary) return search(e->left, salary);
-        return search(e->right, salary);
+    void insert(Emp x) {
+        root = insert(x, root);
     }
 
-    void printEmployeesInRange(int min, int max) {
-        Employee* p = nullptr;
-        Employee* e = root;
-        while (e != nullptr && e->salary != min) {
-            if (min < e->salary) {
-                p = e;
-                e = e->left;
-            }
-            else {
-                p = e;
-                e = e->right;
-            }
-        }
-        if (e == nullptr) e = p;
-        if (e->salary < min) e = successor(e);
-        while (e->salary < max) {
-            cout << e << endl;
-            e = successor(e);
-        }
+    void remove(Emp x) {
+        remove(x, root);
     }
 
-    void printInOrder(Employee* e) {
-        if (e != nullptr) {
-            printInOrder(e->left);
-            cout << *e << endl;
-            printInOrder(e->right);
+    void display() {
+        inorder(root);
+        cout << endl;
+    }
+
+    Emp* search(int x) {
+        node* result = find(root, x);
+        if (result == nullptr) return nullptr;
+        return &result->data;
+    }
+
+    vector<Emp> findAll(int x) {
+        vector<Emp> out;
+        node* y = find(root, x);
+        if (y == nullptr) return out;
+        out.push_back(y->data);
+        while (true) {
+            y = findMin(y->right);
+            if (y == nullptr || y->data.salary != x) break;
+            out.push_back(y->data);
         }
+        return out;
+    }
+
+    void printInRange(int min, int max) {
+        inorderConditional(root, [min, max](node* t) {
+            return t->data.salary >= min && t->data.salary <= max;
+        });
     }
 };
 
@@ -191,7 +237,7 @@ since they are logically associated, this groups them under
 the same namespace. */
 class UI {
 private:
-    EmployeeBST* employees = nullptr;
+    BST* employees = nullptr;
 
     bool isBetween(int num, int* min, int* max) {
         if (min != nullptr && num < *min) return false;
@@ -221,7 +267,7 @@ private:
         return intput;
     }
 
-    Employee inputEmployee() {
+    Emp inputEmployee() {
         int salary;
         string firstName, lastName, jobTitle;
         cout << "Enter a first name: " << endl;
@@ -234,11 +280,11 @@ private:
         int min = 30000;
         int max = 200000;
         salary = inputInteger(&min, &max);
-        return Employee(salary, firstName, lastName, jobTitle);
+        return Emp(firstName, lastName, jobTitle, salary);
     }
 
 public:
-    UI(EmployeeBST* bst): employees(bst) {}
+    UI(BST* bst): employees(bst) {}
 
     void mainMenu() {
         cout << "Welcome to the employee \"database\"" << endl;
@@ -269,7 +315,7 @@ public:
     }
 
     void addEmployee() {
-        Employee e = inputEmployee();
+        Emp e = inputEmployee();
         employees->insert(e);
         cout << "Successfully inserted " << e.firstName << " " << e.lastName << " into the database" << endl;
     }
@@ -279,27 +325,38 @@ public:
         int max = 200000;
         cout << "Select a salary to search for." << endl;
         int salary = inputInteger(&min, &max);
-        Employee* z = employees->search(employees->root, salary);
-        string confirmation;
-        do {
-            if (z == nullptr) {
-                cout << "No employee found with the given salary." << endl;
-                break;
+        vector<Emp> v = employees->findAll(salary);
+        Emp z;
+        int n = v.size();
+        if (n == 0) {
+            cout << "No employees found with the given salary." << endl;
+            return;
+        }
+        else if (n == 1) {
+            z = v.at(0);
+        }
+        else {
+            // Display employees
+            cout << "Found " << n << " employees with salary $" << salary << ". Which would you like to delete?" << endl;
+            for (int i = 0; i < n; i++) {
+                cout << "  " << i+1 << ") " << v.at(i);
             }
-            cout << "Delete employee " << *z << "? (y/n)" << endl;
-            getline(cin, confirmation);
-            if (confirmation == "y") {
-                employees->remove(z);
-                cout << "Successfully deleted " << *z << " from the database" << endl;
-                break;
-            }
-            else if (confirmation == "n") {
-                z = employees->search(z->right, salary);
-            }
-            else {
-                cout << "Invalid input" << endl;
-            }
-        } while (true);
+            // Get user input
+            min = 1;
+            z = v.at(inputInteger(&min, &n) - 1);
+        }
+        cout << "Delete " << z << "?" << endl;
+        cout << "  1) Yes" << endl;
+        cout << "  2) No" << endl;
+        min = 1;
+        max = 2;
+        if (inputInteger(&min, &max) == 1) {
+            employees->remove(z);
+            cout << "Deleted " << z << endl;
+        }
+        else {
+            cout << "Canceled" << endl;
+        }
     }
 
     void searchEmployee() {
@@ -307,21 +364,40 @@ public:
         int min = inputInteger(nullptr, nullptr);
         cout << "Enter a maximum value." << endl;
         int max = inputInteger(&min, nullptr);
-        employees->printEmployeesInRange(min, max);
+        employees->printInRange(min, max);
     }
 };
 
 /* Inserts 10,000 employees into the BST, with random data
 and salaries ranging from 30,000 to 200,000.*/
-void initializeDummyData(EmployeeBST* bst) {
+void initializeDummyData(BST* bst) {
     //Employee e = Employee(30000, "Jonah", "Ebent", "Code Monkey");
     //bst->insert(e);
 }
 
 int main()
 {
-    EmployeeBST bst;
+    BST bst;
     UI ui = UI(&bst);
     initializeDummyData(&bst);
     while (true) ui.mainMenu();
+    //Employee* jonah = new Employee(100000, "J", "A", "E");
+    //bst.insert(Employee(100000, "J", "A", "E"));
+    //bst.printInOrder(bst.root);
+    //BST t;
+    //t.insert(Emp("A", "A", "A", 100000));
+    //t.insert(Emp("B", "B", "B", 150000));
+    //t.insert(Emp("C", "C", "C", 75000));
+    //t.insert(Emp("D", "D", "D", 50000));
+    //t.insert(Emp("E", "E", "E", 200000));
+    //t.insert(Emp("F", "F", "F", 200000));
+    //t.insert(Emp("G", "G", "G", 200000));
+    //t.insert(Emp("H", "H", "H", 200000));
+    //t.display();
+    //Emp a = *t.search(100000);
+    //t.remove(a);
+    //t.display();
+    //vector<Emp> v = t.findAll(200000);
+    //for (Emp e : v) cout << e << endl;
+    //return 0;
 }
